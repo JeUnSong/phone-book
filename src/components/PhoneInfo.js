c0d29c2de76129e96a6fb49e0fed8b5331ea4cc4
JeUnsong <zzangjh5@gmail.com>
2022-04-06 10:11:41 +0900

    튜닝 추가

--- src/components/PhoneInfo.js
+++ src/components/PhoneInfo.js
@@ -51,6 +51,17 @@ class PhoneInfo extends Component {
             })
         }
 
+        shouldComponentUpdate(nextProps, nextState); {
+            // 수정 상태가 아니고, info 값이 같다면 리렌더링 안함
+            if (!this.state.editing
+            && !nextState.editing
+            && nextProps.info === this.props.info) {
+                return false;
+            }
+            // 나머지 경우엔 리렌더링함
+            return true;
+        }
+
         if (prevState.editing && !this.state.editing) {
             // editing 값이 true -> false 로 전환 될 때
             onUpdate(info.id, {
